83b036aa6af316341c6efb89fd2f0c3c5bbb5ae1
rakxya111 <rakshyabhuju@gmail.com>
2025-06-16 23:00:19 +0545

    userCard

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
 
   return (
     <div className="flex flex-1">
-      <div className="home-container ">
+      <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
           {isPostsLoading && !posts ? (
